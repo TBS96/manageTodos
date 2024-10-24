b72af1724ea9ca6d5e2def9eafc1c854d2e5fd62
TBS96 <9tbs6@proton.me>
2024-10-24 21:26:24 +0530

    added functionality for addTodo(eachTodo)
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import { TodoProvider } from './contexts/createTodoContext'
 
 const App = () => {
+
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (eachTodo) => {
+    const newTodo = { id: Date.now(), ...eachTodo };
+    const updatedTodoList = [newTodo, ...todos];
+    setTodos(updatedTodoList)
+  }
+
+  const updateTodo = () => {}
+  
+  const deleteTodo = () => {}
+  
+  const toggleComplete = () => {}
+  
   return (
-    <TodoProvider 
-      // value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}
-    >
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className='bg-[#172842] min-h-screen py-8'>
         <div className='w-full max-w-2xl mx-auto shadow-xl shadow-[#567cb6] rounded-lg px-4 py-3 text-white'>
           <h1 className='text-2xl font-bold text-center mb-8 mt-2'>Manage Your Todos'</h1>
