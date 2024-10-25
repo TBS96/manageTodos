3670a3801533291d287b98006821ea242f0b52f1
TBS96 <9tbs6@proton.me>
2024-10-26 03:30:10 +0530

    added onclick={toggleDone} in editable text input field and some conditional rendering with btns
    when user clicks on the input text area then toggleDone() fn will be triggered
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -22,13 +22,13 @@ const TodoItem = ({eachTodoObj}) => {
         <div className={`flex border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${eachTodoObj.completed ? "bg-[#c6e9a7]" : "bg-[#b57edf]"}`}>
 
             {/* checkbox */}
-            <input type="checkbox" className='cursor-pointer' checked={eachTodoObj.completed} onChange={toggleDone} />
+            <input type="checkbox" className='cursor-pointer accent-red-600' checked={eachTodoObj.completed} onChange={toggleDone} />
 
             {/* editable text */}
-            <input type="search" className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2 bg-white/50" : "border-transparent"} ${eachTodoObj.completed ? "line-through" : ""}`} value={todoMsg} onChange={(e) => setTodoMsg(e.target.value)} readOnly={!isTodoEditable} />
+            <input type="search" className={`border cursor-pointer outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2 bg-white/50" : "border-transparent"} ${eachTodoObj.completed ? "line-through" : ""}`} value={todoMsg} onChange={(e) => setTodoMsg(e.target.value)} readOnly={!isTodoEditable} onClick={toggleDone} />
 
             {/* edit, save btn */}
-            <button className={`inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50 ${isTodoEditable ? "hover:bg-green-300" : "hover:bg-cyan-500"}`} 
+            <button className={`inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 shrink-0 disabled:opacity-50 hover:scale-125 duration-200 hover:shadow-inner hover:shadow-gray-950 ${isTodoEditable ? "hover:bg-green-300" : "hover:bg-cyan-500"}`} 
                 onClick={() => {
                     if (eachTodoObj.completed) return
 
@@ -40,7 +40,7 @@ const TodoItem = ({eachTodoObj}) => {
             disabled={eachTodoObj.completed}>{isTodoEditable ? "💾" : "📝"}</button>
 
             {/* delete btn */}
-            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-red-500 duration-200 shrink-0' onClick={() => deleteTodo(eachTodoObj.id)}>🗑️</button>
+            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-red-500 hover:shadow-inner hover:shadow-gray-950 hover:scale-125 duration-200 shrink-0' onClick={() => deleteTodo(eachTodoObj.id)}>🗑️</button>
 
         </div>
     )
