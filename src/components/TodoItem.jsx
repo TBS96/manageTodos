bdf10b3fe75147a94ffc09d54ed4db7178990efa
TBS96 <9tbs6@proton.me>
2024-10-25 21:41:02 +0530

    destructure from useTodo() hook(context) the functionalities of updateTodo(), deleteTodo(), toggleComplete() and other functionalities
    done conditional rendering
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/createTodoContext'
 
-const TodoItem = () => {
+const TodoItem = ({eachTodoObj}) => {
+
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+    const [todoMsg, setTodoMsg] = useState(eachTodoObj.eachTodo) // eachTodo.eachTodo refers to the eachTodo object's eachTodo property
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+
+    const editTodo = () => {
+        updateTodo(eachTodoObj.id, {...eachTodoObj, eachTodo: todoMsg})
+        setIsTodoEditable(false)
+    }
+
+    const toggleDone = () => {
+        console.log(eachTodoObj)
+        toggleComplete(eachTodoObj.id)
+    }
+
     return (
-        <div className='flex border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black'>
+        <div className={`flex border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${eachTodoObj.completed ? "bg-[#c6e9a7]" : "bg-[#b57edf]"}`}>
 
             {/* checkbox */}
-            <input type="checkbox" className='cursor-pointer' />
+            <input type="checkbox" className='cursor-pointer' checked={eachTodoObj.completed} onChange={toggleDone} />
 
             {/* editable text */}
-            <input type="search" className={`border outline-none w-full bg-transparent rounded-lg`} />
+            <input type="search" className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2 bg-white/50" : "border-transparent"} ${eachTodoObj.completed ? "line-through" : ""}`} value={todoMsg} onChange={(e) => setTodoMsg(e.target.value)} readOnly={!isTodoEditable} />
 
             {/* edit, save btn */}
-            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50'></button>
+            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50' 
+                onClick={() => {
+                    if (eachTodoObj.completed) return
+
+                    if(isTodoEditable) {
+                        editTodo()
+                    }
+                    else setIsTodoEditable((prev) => !prev)
+            }}
+            disabled={eachTodoObj.completed}>{isTodoEditable ? "💾" : "📝"}</button>
 
             {/* delete btn */}
-            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50'></button>
+            <button className='inline-flex size-8 rounded-lg text-sm border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0' onClick={deleteTodo(eachTodo.id)}>🗑️</button>
 
         </div>
     )
