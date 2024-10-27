12613b2403357e92467ba72a7f07ff1e5641fc0d
TBS96 <9tbs6@proton.me>
2024-10-27 23:46:22 +0530

    edit comment
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
         <div className='w-full max-w-2xl mx-auto shadow-xl shadow-[#567cb6] rounded-lg px-4 py-3 text-white'>
           <h1 className='text-2xl font-bold text-center mb-8 mt-2'>Manage Your Todos'</h1>
 
-          {/* TodoForm component below */}
+          {/* TodoForm.jsx component below */}
           <div className='mb-4'>
             <TodoForm onSearch={handleSearch} />
           </div>
